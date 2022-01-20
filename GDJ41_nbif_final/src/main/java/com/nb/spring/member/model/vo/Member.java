6f7bfc9d07e11a759c8481e65a31c911335d3416
DaBinSeo <87168961+DaBinSeo@users.noreply.github.com>
2022-01-20 11:07:31 +0900

    안녕

---
+++ GDJ41_nbif_final/src/main/java/com/nb/spring/member/model/vo/Member.java
@@ -0,0 +1,8 @@
+package com.nb.spring.member.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String name;
+}
---
+++ GDJ41_nbif_final/src/test/java/com/nb/spring/Gdj41NbifFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj41NbifFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
